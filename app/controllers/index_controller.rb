bb129828517db492aad5048f4a622bc8d9349945
Peter Nguyen <peternguyen1991@gmail.com>
2015-03-15 20:18:16 +0700

    employe order by weight

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -5,7 +5,7 @@ class IndexController < ApplicationController
     @aboutus      = Article.first
     @services     = Service.where(isHomePage: true, visible:true).order(updated_at: :asc).limit(4)
     @articles     = Article.where(isHomePage: true, visible:true).order(updated_at: :desc).limit(2)
-    @employees    = Employee.where(isHomePage: true, visible:true).order(updated_at: :asc)
+    @employees    = Employee.where(isHomePage: true, visible:true).order(weight: :asc)
     @testimonials = Testimonial.where(visible: true).order(updated_at: :desc).limit(5)
     @pictures     = Picture.joins(:album).where(visible: true).where(albums: {visible: true, isHomePage: true}).order(updated_at: :desc).limit(27)
     @partners     = Partner.where(isHomePage: true, visible: true).order(updated_at: :asc).limit(4)
